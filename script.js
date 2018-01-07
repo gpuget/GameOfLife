5fe84fe08e0d613faed72174ccabc72656d01037
gpuget <gpuget@excilys.com>
2018-01-07 02:22:56 +0100

    Neighbors grid

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const CANVAS_ID = "myCanvas";
-const CANVAS_W = 50;
-const CANVAS_H = 50;
-const NB_W = 2;
-const NB_H = 2;
+const CANVAS_W = 500;
+const CANVAS_H = 500;
+const NB_W = 50;
+const NB_H = 50;
 const COLOR_ALIVE = "#000";
 const COLOR_DEAD = "#FFF";
-const REFRESH_INTERVAL = 500;
+const REFRESH_INTERVAL = 300;
 
 var grid;
 
@@ -18,7 +18,7 @@ function initCanvas(id) {
 	var container = "body";
 	var canvas = createCanvas(id, CANVAS_W, CANVAS_H);
 	var body = document.getElementsByTagName(container)[0];
-	grid = generateGrid(NB_W, NB_H);
+	generateGrid(NB_W, NB_H);
 
 	body.appendChild(canvas);
 	drawTiles(canvas, grid);
@@ -46,20 +46,74 @@ function drawTiles(canvas, grid) {
 }
 
 function generateGrid(width, height) {
-	var grid = new Array(height);
-	for (var i = 0; i < grid.length; i++) {
-		grid[i] = new Array(width);
-		for (var j = 0; j < grid[i].length; j++) {
+	grid = [];
+	for (var i = 0; i < height; i++) {
+		grid[i] = [];
+		for (var j = 0; j < width; j++) {
 			var cell;
 			if (Math.random() < 0.5) cell = COLOR_DEAD;
 			else cell = COLOR_ALIVE;
-			grid[i][j] = cell;
+			grid[i].push(cell);
 		}
+		grid.push(grid[i]);
 	}
 
 	return grid;
 }
 
 function refreshCanvas() {
+	refreshGrid();
+	drawTiles(document.getElementById(CANVAS_ID), grid);
+}
 
+function neighbors(line, column) {
+	var res = 0;
+
+	for (var i = line - 1; i <= line + 1; i++) {
+		for (var j = column - 1; j <= column +1; j++) {
+			if (i != line || j != column) {
+				if (grid[i] != undefined && grid[i][j] != undefined && grid[i][j] == COLOR_ALIVE) {
+					res++;
+				}				
+			}
+		}
+	}
+
+	return res;
+}
+
+function neighborsGrid() {
+	var ng = [];
+	for (var i = 0; i < grid.length; i++) {
+		var n = [];
+		for (var j = 0; j < grid[i].length; j++) {
+			n.push(neighbors(i, j));
+		}
+		ng.push(n);
+	}
+
+	return ng;
+}
+
+function born(i, j) {
+	grid[i][j] = COLOR_ALIVE;
+}
+
+function die(i, j) {
+	grid[i][j] = COLOR_DEAD;
+}
+
+function refreshGrid() {
+	var ng = neighborsGrid();
+	console.log(ng);
+	for (var i = 0; i < grid.length; i++) {
+		for (var j = 0; j < grid[i].length; j++) {
+			refreshCell(i, j, ng[i][j]);
+		}
+	}
+}
+
+function refreshCell(i, j, n) {
+	if (n == 3) born(i, j);
+	else if (n < 2 || n > 3) die(i, j);
 }
